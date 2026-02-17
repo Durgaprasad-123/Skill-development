8b0cb66f93ca06703ad1d01189fb8662015f7014
DurgaPrasad <durgaprasad.sb9494@gmial.com>
2026-02-17 17:22:11 +0000

    bash day 1

---
+++ 30-days-bash/day1/host_date_users.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "The hostname is : $(hostname)"
+echo "The date is : $(date)"
+echo "The Logged in users using w command: $(w)"
---
+++ 30-days-bash/day1/sysinfo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo -e "the system info of this os: \n $(hostnamectl)"
+
+
